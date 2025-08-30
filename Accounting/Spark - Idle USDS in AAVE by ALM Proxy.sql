4720e85ffa8672e319e526f3f72acc205e4fa230
Walledao <1275511590@qq.com>
2025-08-31 07:09:01 +0800

    initial

---
+++ Accounting/Spark - Idle USDS in AAVE by ALM Proxy.sql
@@ -0,0 +1,161 @@
+-- header
+with
+    spark_tokens (blockchain, category, symbol, contract_address, decimals, start_date) as (
+        values
+        ('ethereum', 'aToken', 'USDS', 0x09AA30b182488f769a9824F15E6Ce58591Da4781, 18, date '2024-10-02'),   -- aEthLidoUSDS
+        ('ethereum', 'debtToken', 'USDS', 0x2D9fe18b6c35FE439cC15D932cc5C943bf2d901E, 18, date '2024-10-02') -- variable Debt EthLidoUSDS
+    ),
+    -- get total supply and borrow amounts
+    sp_transfers_totals as (
+        select
+            dt,
+            symbol,
+            sum(if(category = 'aToken', amount, 0)) as supply_amount,
+            sum(if(category = 'debtToken', amount, 0)) as borrow_amount
+        from (
+            -- ********************************** Mints/Burns aToken **********************************
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                ((cast(t."value" as int256) - cast(t.balanceIncrease as int256)) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_mint t
+            join spark_tokens st using (contract_address)
+            where st.category = 'aToken'
+            union all
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                -((t."value" + t.balanceIncrease) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_burn t
+            join spark_tokens st using (contract_address)
+            where st.category = 'aToken'
+            union all
+            -- ********************************** Mints/Burns debtTokens **********************************
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                ((cast(t."value" as int256) - cast(t.balanceIncrease as int256)) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_mint t
+            join spark_tokens st using (contract_address)
+            where st.category = 'debtToken'
+            union all
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                -((t."value" + t.balanceIncrease) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_burn t
+            join spark_tokens st using (contract_address)
+            where st.category = 'debtToken'
+        )
+        group by 1,2
+    ),
+    -- get total supply amount for ALM Proxy
+    sp_transfers_alm as (
+        select
+            symbol,
+            dt,
+            sum(amount) as alm_supply_amount
+        from (
+            -- ********************************** Mints/Burns aToken **********************************
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                ((cast(t."value" as int256) - cast(t.balanceIncrease as int256)) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_mint t
+            join spark_tokens st using (contract_address)
+            where st.category = 'aToken'
+              and t.onBehalfOf = 0x1601843c5E9bC251A3272907010AFa41Fa18347E -- ALM Proxy
+              and t.evt_block_date >= st.start_date
+            union all
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                -((t."value" + t.balanceIncrease) / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_burn t
+            join spark_tokens st using (contract_address)
+            where st.category = 'aToken'
+              and t.target = 0x1601843c5E9bC251A3272907010AFa41Fa18347E -- ALM Proxy
+              and t.evt_block_date >= st.start_date
+            union all
+            -- ********************************** Transfers aToken **********************************
+            -- @dev: the ALM Proxy contract does not have direct transfers, but just in case
+            select
+                t.evt_block_date as dt,
+                st.category,
+                st.symbol,
+                if("to" = 0x1601843c5E9bC251A3272907010AFa41Fa18347E, 1, -1) * (t."value" / 1e18) / (index / 1e27) as amount
+            from aave_v3_lido_ethereum.atoken_evt_balancetransfer t
+            join spark_tokens st using (contract_address)
+            where st.category = 'aToken'
+              and 0x1601843c5E9bC251A3272907010AFa41Fa18347E in ("from", "to") -- ALM Proxy
+              and t.evt_block_date >= st.start_date
+        )
+        group by 1,2
+    ),
+    seq as (
+        select
+            t.symbol,
+            s.dt
+        from (select symbol, min(dt) as start_dt from sp_transfers_totals group by 1) t
+        cross join unnest(sequence(t.start_dt, current_date, interval '1' day)) as s(dt)
+    ),
+    -- get cumulative balances
+    sp_balances as (
+        select
+            dt,
+            symbol,
+            sum(coalesce(ta.alm_supply_amount, 0)) over (partition by symbol order by dt) as alm_supply_amount,
+            sum(coalesce(tt.supply_amount, 0)) over (partition by symbol order by dt) as supply_amount,
+            sum(coalesce(tt.borrow_amount, 0)) over (partition by symbol order by dt) as borrow_amount
+        from seq s
+        left join sp_transfers_alm ta
+            using (dt, symbol)
+        left join sp_transfers_totals tt
+            using (dt, symbol) 
+    ),
+    -- get underlying amounts through indices
+    sp_balances_index as (
+        select
+            b.dt,
+            'ethereum' as blockchain,
+            'AAVE ALM Proxy' as protocol_name,
+            b.symbol as token_symbol,
+            i.supply_index,
+            i.borrow_index,
+            b.alm_supply_amount * i.supply_index as alm_supply_amount,
+            b.supply_amount * i.supply_index as supply_amount,
+            b.borrow_amount * i.borrow_index as borrow_amount,
+            (b.supply_amount * i.supply_index) - (b.borrow_amount * i.borrow_index) as idle_amount
+        from sp_balances b
+        left join dune.steakhouse.result_spark_protocol_markets_data i
+            on b.symbol = i.symbol
+            and b.dt = i."period"
+        where i.symbol in ('DAI', 'USDS')
+    ),
+    -- get the reward amounts
+    sp_balances_rates as (
+        select
+            b.*,
+            (alm_supply_amount / supply_amount) as alm_share,
+            r.reward_code,
+            r.reward_per,
+            i.reward_code as interest_code,
+            -i.reward_per as interest_per,
+            (idle_amount * (alm_supply_amount / supply_amount)) as alm_idle
+        from sp_balances_index b
+        cross join query_5353955 r -- Spark - Accessibility Rewards - Rates
+        cross join query_5353955 i -- Spark - Accessibility Rewards - Rates => interest percentage
+        where r.reward_code = 'AR'
+          and i.reward_code = 'BR'
+          and b.dt between r.start_dt and r.end_dt
+          and b.dt between i.start_dt and i.end_dt
+
+    )
+    
+select * from sp_balances_rates order by dt desc
