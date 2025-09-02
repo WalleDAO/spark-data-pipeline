c1eb5a70e90f102ff8410d69f22adf2757f583c0
Walledao <1275511590@qq.com>
2025-09-02 14:44:52 +0800

    APR

--- Accounting/Spark - Idle DAI & USDS in Sparklend by ALM Proxy.sql
+++ Accounting/Spark - Idle DAI & USDS in Sparklend by ALM Proxy.sql
@@ -350,7 +350,7 @@ with
             s.alm_idle,
             s.reward_code,
             s.reward_per,
-            'APY-BR' as interest_code,
+            'APR-BR' as interest_code,
             s.borrow_rate_apr-s.reward_per as interest_per,  -- Use APR for interest calculation
             rf.reserve_factor,
             s.sofr,
